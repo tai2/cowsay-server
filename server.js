09a44562a8e84467106b4d81d01e14b6d2915aed
tai2 <hello@tai2.net>
2017-01-29 17:35:43 +0900

    Initial import.

---
+++ server.js
@@ -0,0 +1,24 @@
+const http = require('http');
+const parse = require('url').parse;
+const cowsay = require('cowsay');
+const server = http.createServer(function (req, res) {
+    const url = parse(req.url, true);
+    const msg = url.query.msg || 'Hello Node.js!';
+    const cow = cowsay.say({text: msg});
+    const content = `<html>
+    <head>
+        <title>cowsay</title>
+        <link href="https://fonts.googleapis.com/css?family=Inconsolata" rel="stylesheet">
+        <style>
+            pre {
+                font-family: 'Inconsolata', monospace;
+            }
+        </style>
+    </head>
+    <body>
+        <pre>${cow}</pre>
+    </body>
+</html>`;
+    res.end(content);
+})
+server.listen(8000)
